7d6f0eb6cebfe6f6996625d3a07aebd9fd9b96a1
YoungDavi <davidyoung21071980@gmail.com>
2025-03-26 10:04:31 +0100

    Update app.js
    add guardienjs

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const helmet = require('helmet');
 const routes = require('./routes');
-var compression = require('compression')
+const compression = require('compression')
+import { GuardianJS } from '@guardianjs/core';
 
 const app = express();
+const config: GuardianConfig = {
+  threshold: 0.8,
+  enableBehaviorAnalysis: true,
+  enableTLSFingerprinting: true,
+  // ... other options
+};
+
+const guardian = new GuardianJS(config);
+app.use(guardian.middleware());
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
